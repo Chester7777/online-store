1af819e2f247771323fb399a9b04210d37937847
Es180 <Es18.03.88@gmail.com>
2021-09-04 22:54:48 +0300

    server

--- Server/controllers/userController.js
+++ Server/controllers/userController.js
@@ -1,6 +1,15 @@
 const ApiError = require("../Error/ApiError")
 const bcrypt = require("bcrypt")
+const jwt = require("jsonwebtoken")
 const {User, Basket } = require("../models/models")
+
+const generateJwt = () => {
+return jwt.sign(
+    {id, email, role},
+    process.env.SECRET_KEY,
+    {expiresIn: "24h"}
+)
+}
 
 
 class UserController {
@@ -13,19 +22,36 @@ const {email, password, role} = req.body
         if(candidate) {
             return next(ApiError.badRequest("Пользователь с таким email уже существует"))
         }
+        const hashPassword = await bcrypt.hash(password, 5)
+        const user = await User.create({email, role, password: hashPassword})
+        const basket = await Basket.create({userId: user.id})
+        const token = generateJwt(user.id, user.email, user.role)
+        return res.json({token})
     }
 
 
-    async login(req, res) {
-
+    async login(req, res, next) {
+const {email, password} = req.body
+        const user = await User.findOne({where: {email}})
+        if(!user) {
+            return next(ApiError.internal("Пользователь не найден"))
+        }
+        let comparePassword = bcrypt.compareSync(password, user.password)
+        if(!comparePassword) {
+            return next(ApiError.internal("Указан не верный пароль"))
+        }
+        const token = generateJwt(user.id, user.email, user.role)
+return res.json({token})
     }
 
     async check(req, res, next) {
-        const {id} = req.query
-        if (!id) {
-            return next(ApiError.badRequest("не задан id"))
-        }
-        res.json(id)
+        // const {id} = req.query
+        // if (!id) {
+        //     return next(ApiError.badRequest("не задан id"))
+        // }
+        // res.json(id)
+        const token = generateJwt(req.user.id, req.user.email, req.user.role)
+        return res.json({token})
     }
 }
 
